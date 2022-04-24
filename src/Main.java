1b18ee5071d8106b24b464045076f49be44f7954
daisgu014 <daibucky@gmail.com>
2022-04-24 22:46:53 +0700

    Nguyễn Hữu Đại

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello mấy cưng");
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+
+
+    }
 }
